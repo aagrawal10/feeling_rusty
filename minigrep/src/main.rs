54d03fb462ddff0e4b35a90f7fcd92709827d91a
Akash Agrawal <akash@coefficient.io>
2021-11-17 06:20:45 +0530

    Changes after reading the chapter

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::io;
+use std::error;
 use std::fs;
+use std::process;
 
 
 struct Config {
@@ -12,7 +13,8 @@ struct Config {
 fn parse_args() -> Config {
     let arguments: Vec<String> = env::args().collect();
     if arguments.len() != 3 {
-        panic!("Usage: cargo minigrep <query> <filename>");
+        println!("Usage: cargo minigrep <query> <filename>");
+        process::exit(-1);
     }
 
     return Config{
@@ -22,20 +24,20 @@ fn parse_args() -> Config {
 }
 
 
-fn get_file_as_str(filename: &str) -> io::Result<String> {
+fn get_file_as_str(filename: &str) -> Result<String, Box<dyn error::Error>> {
     let contents = fs::read_to_string(filename)?;
     return Ok(contents);
 }
 
 
-fn minigrep<'a>(contents: &'a str, query: &'a str) -> Vec<&'a str> {
+fn minigrep<'a>(contents: &'a str, query: & str) -> Vec<&'a str> {
     let mut result: Vec<&str> = Vec::new();
     for line in contents.lines() {
         if !line.to_lowercase().contains(&query.to_lowercase()) {
             continue;
         }
 
-        result.push(&line);
+        result.push(line);
     }
 
     return result;
@@ -44,7 +46,10 @@ fn minigrep<'a>(contents: &'a str, query: &'a str) -> Vec<&'a str> {
 
 fn main() {
     let config = parse_args();
-    let contents = get_file_as_str(&config.filename).expect("Error: while reading from file");
+    let contents = get_file_as_str(&config.filename).unwrap_or_else(|err| {
+        eprintln!("Error: {} while reading from file {}", err, config.filename);
+        process::exit(-1);
+    });
     let result = minigrep(&contents, &config.query);
     if result.len() == 0 {
         println!("No matching rows found!!");
